f2289a9158b6cbf30f9024c03da889e4e4a089bf
tozastation <b1016031@fun.ac.jp>
2019-04-02 22:44:45 +0900

    [Parts]: add user repository implement

---
+++ src/main/scala/com/github/tozastation/clasickcoreapi/infrastructure/persistence/repository/UserRepositoryImpl.scala
@@ -0,0 +1,37 @@
+package com.github.tozastation.clasickcoreapi.infrastructure.persistence.repository
+
+import com.github.tozastation.clasickcoreapi.domain.model._
+import com.github.tozastation.clasickcoreapi.domain.repository.{UserRepository, UsesUserRepository}
+import com.github.tozastation.clasickcoreapi.infrastructure.persistence.component.MySQLDBComponent
+import slick.jdbc.MySQLProfile.api._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+/**
+  * MixInUserRepository
+  * UserRepositoryの実装を提供するただ一つのフィールドを持つモジュール
+  */
+trait MixInUserRepository extends UsesUserRepository {
+  val userRepository = UserRepositoryImpl
+}
+
+
+object UserRepositoryImpl extends UserRepository {
+  override def selectUser(id: UserId): Future[Option[User]] = {
+    val q = Users.filter(_.id === id)
+    MySQLDBComponent.db.run(q.result.headOption)
+  }
+
+  override def createUser(user: User): Future[Option[AccessToken]] = {
+    val result = MySQLDBComponent.db.run(Users += user)
+    result.transform(
+      {_ => return Future(Option(user.accessToken)) },
+      {_ => throw new Exception("Can't Insert Data")}
+    )
+  }
+
+  override def selectMe(name: UserName, pass: UserPass): Future[Option[User]] = {
+    val q = Users.filter(u => u.name === name && u.pass === pass)
+    MySQLDBComponent.db.run(q.result.headOption)
+  }
+}
